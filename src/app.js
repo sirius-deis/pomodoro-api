d35e4e5affbcdf434489c6af2e60e9e94dbe83da
Sirius Deis <falebsy@gmail.com>
2023-05-14 23:31:51 +0200

    add middlewares to app and start function to start the server

---
+++ src/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParse = require('cookie-parser');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
+const { log } = require('mercedlogger');
+
+const app = express();
+
+const corsOptions = {
+    origin: true,
+    credentials: true,
+};
+
+const limiter = rateLimit({
+    max: 1000,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour',
+});
+
+app.use(morgan('tiny'));
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParse());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(limiter);
+
+const { PORT = 3002 } = process.env.PORT;
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(
+                log.green('SERVER STATE', `Server is running on port: ${PORT}`)
+            );
+        });
+    } catch (error) {
+        console.log(log.red('SERVER STATE', error));
+        process.exit(1);
+    }
+};
+
+start();
